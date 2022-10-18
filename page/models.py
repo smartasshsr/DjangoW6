d508f191c19c172bf4c4ca5c5a7118463c5de9af
JG-C3 <jgc3django@gmail.com>
2022-10-18 23:58:37 +0900

    Update Django W6

--- page/models.py
+++ page/models.py
@@ -6,3 +6,6 @@ class Posting(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
