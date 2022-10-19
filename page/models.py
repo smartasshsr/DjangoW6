71fbf75cbc86bc0231c805426ca06072bfaec063
JG-C3 <jgc3django@gmail.com>
2022-10-19 14:08:44 +0900

    Update Django W6

--- page/models.py
+++ page/models.py
@@ -6,3 +6,6 @@ class Posting(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    # [코드 작성] admin 페이지에서 Posting 객체들이 title로 보여지도록 코드 작성
+    
